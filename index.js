4abb771fe34df394eca19c614c7d609dd3915daf
Dina Hafez <dinahafez.8@gmail.com>
2018-05-31 13:26:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -171,6 +171,14 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   function step() {
+     var leftNumbers = DODGER.style.left.replace('px', '');
+     var left = parseInt(leftNumbers, 10);
+     if(left > 0) {
+       DODGER.style.left = `${left + 4}px`
+     }
+   }
+   window.requestAnimationFrame(step);
    
 }
 
